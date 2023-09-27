aa9c9c437cea77421d76c79d5291dc0ffb3ded20
Niklas Lausch <niki@lauschig.net>
2023-09-27 16:41:36 +0200

    Security - Added apiKey - Added python-dotenv env loading

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
 import uuid
 import whisper
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_keys = os.environ['API_KEYS'].split(',')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key not in api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden"
+        )
+
 
 app = FastAPI()
 
 
-@app.post("/transcribe")
+@app.post("/transcribe", dependencies=[Depends(api_key_auth)])
 async def transcibe(request: Request):
     data: bytes = await request.body()
 
